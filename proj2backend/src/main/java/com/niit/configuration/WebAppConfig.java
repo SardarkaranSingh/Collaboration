8da9a81daddd0e133a9f6edf26883e630922c8f8
Admin <Admin@172.45.45.158>
2018-03-14 15:34:04 +0530

    profile picture can now be uploded by User/admin

--- proj2backend/src/main/java/com/niit/configuration/WebAppConfig.java
+++ proj2backend/src/main/java/com/niit/configuration/WebAppConfig.java
@@ -1,7 +1,11 @@
 package com.niit.configuration;
 
+import org.springframework.context.annotation.Bean;
+
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -15,5 +19,13 @@ public class WebAppConfig extends WebMvcConfigurerAdapter
 	{
 		System.out.println("webappconfig is instantiated");
 	}
+	
+	@Bean(name="multipartResolver")
+    public CommonsMultipartResolver commonsMultipartResolver() 
+    {
+    	CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+    	return commonsMultipartResolver;	
+    }
+
 
 }
